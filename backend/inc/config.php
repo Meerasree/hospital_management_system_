40e9644204b288151ebd717baf61e3424a7084ec
Meerasree <meera332004@gmail.com>
2025-11-25 11:02:57 +0530

    Add files via upload

---
+++ backend/inc/config.php
@@ -0,0 +1,8 @@
+<?php
+$dbuser="root";
+$dbpass="";
+$host="localhost";
+$db="hospital_management_system";
+$mysqli=new mysqli($host,$dbuser, $dbpass, $db);
+?>
+
---
+++ backend/inc/footer1.php
@@ -0,0 +1,4 @@
+<footer class="footer footer-alt">
+    <?php echo date('Y'); ?> &copy; Hospital Management Information System. Developed By <a href="">Meera</a></a>
+
+</footer>
